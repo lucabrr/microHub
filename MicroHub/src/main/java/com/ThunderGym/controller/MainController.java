705f7df17fd06e6a0ca9e5989ee540097377b4b5
lucabrr <lucabrancato95@gmail.com>
2023-11-06 18:14:28 +0100

    cross origin

--- MicroHub/src/main/java/com/ThunderGym/controller/MainController.java
+++ MicroHub/src/main/java/com/ThunderGym/controller/MainController.java
@@ -28,108 +28,113 @@ import com.ThunderGym.service.ClienteService;
 import com.ThunderGym.service.FatturaService;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://thundergym-8e9b5.web.app/", maxAge = 3600)
 @RequestMapping("/api/dashboard")
 public class MainController {
-	
-	@Autowired ClienteService cs;
-	@Autowired FatturaService fs;
-	
+
+	@Autowired
+	ClienteService cs;
+	@Autowired
+	FatturaService fs;
+
 	@GetMapping("/clientSummary")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<HashMap<String,Integer>> getClientSummary(){
-		HashMap<String,Integer> clientSummary = new HashMap<String, Integer>();
+	public ResponseEntity<HashMap<String, Integer>> getClientSummary() {
+		HashMap<String, Integer> clientSummary = new HashMap<String, Integer>();
 		clientSummary.put("clientiTotali", cs.getAllCLienti().size());
 		clientSummary.put("clientiValidi", cs.getClientiValidi().size());
 		clientSummary.put("clienti_inScadenza", cs.getClienteInScandenza().size());
 		clientSummary.put("clientiInattivi", cs.getClientiInattivi().size());
-		return new ResponseEntity<HashMap<String,Integer>>(clientSummary,HttpStatus.ACCEPTED);
-		
+		return new ResponseEntity<HashMap<String, Integer>>(clientSummary, HttpStatus.ACCEPTED);
+
 	};
-	
-	
+
 	@GetMapping("/clienteId/{id}")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Cliente> getById(@PathVariable Long id){
-		return new ResponseEntity<Cliente>(cs.trovaDaId(id),HttpStatus.ACCEPTED);
+	public ResponseEntity<Cliente> getById(@PathVariable Long id) {
+		return new ResponseEntity<Cliente>(cs.trovaDaId(id), HttpStatus.ACCEPTED);
 	}
-	
-	
-	
-	
+
 	@PostMapping("/inactiveClientiPageable")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Page<Cliente>> getNuberOfInactiveClientiPageable(Pageable page){
-		return new ResponseEntity<Page<Cliente>>(cs.getClientiInattiviPageable(page),HttpStatus.ACCEPTED);
+	public ResponseEntity<Page<Cliente>> getNuberOfInactiveClientiPageable(Pageable page) {
+		return new ResponseEntity<Page<Cliente>>(cs.getClientiInattiviPageable(page), HttpStatus.ACCEPTED);
 	}
+
 	@GetMapping("/importi")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<HashMap<String,Double>> getImporti(){
-		return new ResponseEntity<HashMap<String,Double>>(fs.getImportiMeseCorrenteNpassato(),HttpStatus.ACCEPTED);
+	public ResponseEntity<HashMap<String, Double>> getImporti() {
+		return new ResponseEntity<HashMap<String, Double>>(fs.getImportiMeseCorrenteNpassato(), HttpStatus.ACCEPTED);
 	}
+
 	@GetMapping("/iscritti")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<HashMap<String,Integer>> getIscritti(){
-		return new ResponseEntity<HashMap<String,Integer>>(cs.getIscrittiMesePassatoECorrente(),HttpStatus.ACCEPTED);
+	public ResponseEntity<HashMap<String, Integer>> getIscritti() {
+		return new ResponseEntity<HashMap<String, Integer>>(cs.getIscrittiMesePassatoECorrente(), HttpStatus.ACCEPTED);
 	}
+
 	@PostMapping("/findCliente")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Page<Cliente>> findCliente(Pageable page, @RequestBody Cliente cliente){
-		
-		return new ResponseEntity<Page<Cliente>>(cs.clienteNomeSimile(cliente.getNome(),page),HttpStatus.ACCEPTED);
+	public ResponseEntity<Page<Cliente>> findCliente(Pageable page, @RequestBody Cliente cliente) {
+
+		return new ResponseEntity<Page<Cliente>>(cs.clienteNomeSimile(cliente.getNome(), page), HttpStatus.ACCEPTED);
 	}
+
 	@PostMapping("/clienteInScadenza")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<Page<Cliente>> findClienteInScandenza(Pageable page){
-		return new ResponseEntity<Page<Cliente>>(cs.getClienteInScandenzaPageable(page),HttpStatus.ACCEPTED);
+	public ResponseEntity<Page<Cliente>> findClienteInScandenza(Pageable page) {
+		return new ResponseEntity<Page<Cliente>>(cs.getClienteInScandenzaPageable(page), HttpStatus.ACCEPTED);
 	}
+
 	@PostMapping("/newCliente")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<String> creaCliente(@RequestBody Cliente cliente){
+	public ResponseEntity<String> creaCliente(@RequestBody Cliente cliente) {
 		try {
 			cliente.setDataIscrizione(LocalDate.now());
 			cliente.setDataUltimoIngresso(LocalDate.now());
-			cliente.setNomeCompleto(cliente.getNome()+cliente.getCognome());
-			
+			cliente.setNomeCompleto(cliente.getNome() + cliente.getCognome());
+
 			cs.salvaCliente(cliente);
 			Fattura f = fs.creaFattura(cliente);
 			fs.salvaFattura(f);
-			String message = "Cliente " + cliente.getNome()+ " " + cliente.getCognome()+ " " +"creato";
-			return new ResponseEntity<String>(message,HttpStatus.ACCEPTED);
+			String message = "Cliente " + cliente.getNome() + " " + cliente.getCognome() + " " + "creato";
+			return new ResponseEntity<String>(message, HttpStatus.ACCEPTED);
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
-					
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+
 		}
 	}
+
 	@PostMapping("/modificaClient")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<?> findCliente(@RequestBody Cliente cliente){
+	public ResponseEntity<?> findCliente(@RequestBody Cliente cliente) {
 		try {
-			
+
 			Cliente c = cs.salvaCliente(cliente);
-			
-			return new ResponseEntity<Cliente>(c,HttpStatus.ACCEPTED);
+
+			return new ResponseEntity<Cliente>(c, HttpStatus.ACCEPTED);
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
 	}
+
 	@PatchMapping("/rinnovaIngresso/{id}")
 	@PreAuthorize("hasRole('ADMIN')")
-	public ResponseEntity<?>rinnovaIngressoCliente(@PathVariable long id, @RequestBody Cliente clienteIngresso){
+	public ResponseEntity<?> rinnovaIngressoCliente(@PathVariable long id, @RequestBody Cliente clienteIngresso) {
 		try {
 			Cliente cliente = cs.trovaDaId(id);
-	
-			cliente.setIngresso(clienteIngresso.getIngresso()+cliente.getIngresso());
+
+			cliente.setIngresso(clienteIngresso.getIngresso() + cliente.getIngresso());
 			Cliente clienteDaTornare = cs.salvaCliente(cliente);
 			Fattura f = fs.creaFatturaManuale(cliente, clienteIngresso.getIngresso());
 			fs.salvaFattura(f);
 			return new ResponseEntity<Cliente>(clienteDaTornare, HttpStatus.ACCEPTED);
-			
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			return new ResponseEntity<String>(e.getMessage(),HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
 		}
-		
+
 	}
-	
- }
+
+}
