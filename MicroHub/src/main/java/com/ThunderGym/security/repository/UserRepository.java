3aca2c6b14919b6b23690aa7960009b4b0603fac
lucabrr <lucabrancato95@gmail.com>
2024-01-04 12:19:13 +0100

    aggiunta annotazione repository a userRepo

--- MicroHub/src/main/java/com/ThunderGym/security/repository/UserRepository.java
+++ MicroHub/src/main/java/com/ThunderGym/security/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package com.ThunderGym.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ThunderGym.security.entity.User;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
